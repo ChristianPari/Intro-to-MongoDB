206e9e49521ff6c739e0ccb0168939977f4863f6
Christian Pari <ChristianPari@outlook.com>
2020-06-03 13:53:55 -0400

    Moved DB connect to middleware file

--- First-Server-002/index.js
+++ First-Server-002/index.js
@@ -4,12 +4,15 @@ const express = require('express'),
     dbConnect = require('./dbConnection'),
     spacer = require('./spacer'),
     app = express(),
-    port = process.env.PORT || 5000;
+    port = process.env.PORT || 5000,
+    homeRouter = require('./routes/homeRouter');
 
 app.use(morgan("dev"));
 app.use(express.json());
 
 dbConnect();
+
+app.use('/', homeRouter);
 
 app.listen(port, () => {
 
